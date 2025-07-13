a3c18e8c605e4fae7d7c82449df5ac5d4760af8a
Brian Cabrera <briancabrer4@gmail.com>
2025-07-13 16:58:02 -0300

    Add Jackson configuration to support Java 8 date/time types

---
+++ src/main/java/com/briancabrera/teamtasks/infrastructure/config/JacksonConfig.java
@@ -0,0 +1,20 @@
+package com.briancabrera.teamtasks.infrastructure.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Global configuration for Jackson to support Java 8 date/time types like LocalDate and LocalDateTime.
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
