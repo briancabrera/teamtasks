87b720ca6a3ac4d33ba51ae2a21a1eb47052054c
Brian Cabrera <briancabrer4@gmail.com>
2025-07-13 14:28:42 -0300

    setup

---
+++ src/main/java/com/briancabrera/teamtasks/TeamTasksApplication.java
@@ -0,0 +1,11 @@
+package com.briancabrera.teamtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTasksApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamTasksApplication.class, args);
+    }
+}
